99965b32aa584579e720e54dda93e461a1553fdc
bmsk <vv99911@gmail.com>
2023-05-09 09:06:45 +0900

    Update SettingActivity.kt
    코드 리펙터링

--- app/src/main/java/org/bmsk/weatherwise/ui/SettingActivity.kt
+++ app/src/main/java/org/bmsk/weatherwise/ui/SettingActivity.kt
@@ -1,16 +1,15 @@
 package org.bmsk.weatherwise.ui
 
-import android.Manifest
 import android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
 import android.Manifest.permission.POST_NOTIFICATIONS
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import org.bmsk.weatherwise.R
 import org.bmsk.weatherwise.UpdateWeatherService
@@ -27,17 +26,24 @@ class SettingActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.settingButton.setOnClickListener {
-            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-                data = Uri.fromParts("package", packageName, null)
-            }
-            startActivity(intent)
-            finish()
+            openAppSettings()
         }
     }
 
     override fun onStart() {
         super.onStart()
+        requestPermissionsByVersion()
+    }
 
+    private fun openAppSettings() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            data = Uri.fromParts("package", packageName, null)
+        }
+        startActivity(intent)
+        finish()
+    }
+
+    private fun requestPermissionsByVersion() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             locationPermissionRequest.launch(
                 arrayOf(
@@ -57,19 +63,27 @@ class SettingActivity : AppCompatActivity() {
     ) { permissions ->
         when {
             permissions.getOrDefault(ACCESS_BACKGROUND_LOCATION, false) -> {
-                ContextCompat.startForegroundService(
-                    this,
-                    Intent(this, UpdateWeatherService::class.java)
-                )
+                startUpdateWeatherService()
             }
 
             else -> {
-                Toast.makeText(
-                    this,
-                    getString(R.string.need_location_permission),
-                    Toast.LENGTH_SHORT
-                ).show()
+                showLocationPermissionToast()
             }
         }
+    }
+
+    private fun startUpdateWeatherService() {
+        ContextCompat.startForegroundService(
+            this,
+            Intent(this, UpdateWeatherService::class.java)
+        )
+    }
+
+    private fun showLocationPermissionToast() {
+        Toast.makeText(
+            this,
+            getString(R.string.need_location_permission),
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
